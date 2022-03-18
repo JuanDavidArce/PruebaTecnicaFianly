0176ebd16fa09ce09468b2a92c65e598d85e35a9
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-03-18 11:17:48 -0500

    utilities verification user exists

--- TechnicalTest/utilities/generate_data.py
+++ TechnicalTest/utilities/generate_data.py
@@ -7,11 +7,13 @@ def save_data():
                             quoting=csv.QUOTE_MINIMAL)
         for row in reader:
             if row != ['first_name', 'last_name', 'password', 'username', 'email']:
-                User.objects.create_user(
+                if not User.objects.get(email=row[4]):
+                    User.objects.create_user(
                     first_name=row[0],
                     last_name=row[1],
                     password=row[2],
                     username=row[3],
                     email=row[4],
                     is_active = True,
-                )
+                    )
+                
