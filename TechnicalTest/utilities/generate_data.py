fbcca8840b6471060c290794b584cdfddfaf5b3b
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-03-18 11:05:21 -0500

    utilities to generate data

---
+++ TechnicalTest/utilities/generate_data.py
@@ -0,0 +1,17 @@
+import csv
+from users.models import User 
+
+def save_data():
+    with open('./utilities/data.csv') as File:
+        reader = csv.reader(File, delimiter=',', quotechar=',',
+                            quoting=csv.QUOTE_MINIMAL)
+        for row in reader:
+            if row != ['first_name', 'last_name', 'password', 'username', 'email']:
+                User.objects.create_user(
+                    first_name=row[0],
+                    last_name=row[1],
+                    password=row[2],
+                    username=row[3],
+                    email=row[4],
+                    is_active = True,
+                )
