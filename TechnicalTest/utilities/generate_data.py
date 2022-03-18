59df3ce93dcd0366ec29274ee28221e5e56a5417
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-03-18 11:21:21 -0500

    utilities updated

--- TechnicalTest/utilities/generate_data.py
+++ TechnicalTest/utilities/generate_data.py
@@ -7,13 +7,16 @@ def save_data():
                             quoting=csv.QUOTE_MINIMAL)
         for row in reader:
             if row != ['first_name', 'last_name', 'password', 'username', 'email']:
-                if not User.objects.get(email=row[4]):
+                try:
+                    User.objects.get(email=row[4])
+                
+                except:
                     User.objects.create_user(
-                    first_name=row[0],
-                    last_name=row[1],
-                    password=row[2],
-                    username=row[3],
-                    email=row[4],
-                    is_active = True,
-                    )
+                        first_name=row[0],
+                        last_name=row[1],
+                        password=row[2],
+                        username=row[3],
+                        email=row[4],
+                        is_active = True,
+                        )
                 
